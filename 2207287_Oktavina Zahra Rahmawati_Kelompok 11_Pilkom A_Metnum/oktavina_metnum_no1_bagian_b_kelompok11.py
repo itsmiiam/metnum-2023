55965e447578c48a899c8b77e36db491aa3572c8
oktavinazr <145572147+oktavinazr@users.noreply.github.com>
2023-09-21 12:43:19 +0700

    Update oktavina_metnum_no1_bagian_b_kelompok11.py

--- 2207287_Oktavina Zahra Rahmawati_Kelompok 11_Pilkom A_Metnum/oktavina_metnum_no1_bagian_b_kelompok11.py
+++ 2207287_Oktavina Zahra Rahmawati_Kelompok 11_Pilkom A_Metnum/oktavina_metnum_no1_bagian_b_kelompok11.py
@@ -1,15 +1,20 @@
 '''
-no. 1
+Oktavina Zahra Rahmawati (2207287)
+No. 1
 b. f(x) = e^x - x
 '''
+
 import numpy as np
 
+# Fungsi untuk mencari akar dengan metode biseksi
 def my_bisection(func, a, b, tol=1e-6, max_iterations=100):
     iterations = 0
     while iterations < max_iterations:
         c = (a + b) / 2
+        # Memeriksa apakah akar ditemukan atau toleransi terpenuhi
         if func(c) == 0 or (b - a) / 2 < tol:
             return c
+        # Memeriksa tanda fungsi untuk menentukan interval berikutnya
         if np.sign(func(c)) == np.sign(func(a)):
             a = c
         else:
@@ -17,13 +22,15 @@ def my_bisection(func, a, b, tol=1e-6, max_iterations=100):
         iterations += 1
     return None
 
-# Fungsi baru: f(x) = e^x - x
+# Fungsi yang akan dicari akarnya: f(x) = e^x - x
 f = lambda x: np.exp(x) - x
 
+# Mencari akar dengan toleransi 0.1
 r1 = my_bisection(f, 0, 2, 0.1)
 print("r1 =", r1)
 print("f(r1) =", f(r1))
 
+# Mencari akar dengan toleransi 0.01
 r01 = my_bisection(f, 0, 2, 0.01)
 print("r01 =", r01)
 print("f(r01) =", f(r01))
