f7810404ff5f1851881e642a4321976a3340e54c
Elliora Orva Linnisa <145202474+ellioraorvalinnisa@users.noreply.github.com>
2023-09-21 11:38:22 +0700

    Update Elliora_Metnum_no2_kelompok11.py

--- 2203451_EllioraOrvaLinnisa_Kelompok11_Pilkom3A_Metnum/Elliora_Metnum_no2_kelompok11.py
+++ 2203451_EllioraOrvaLinnisa_Kelompok11_Pilkom3A_Metnum/Elliora_Metnum_no2_kelompok11.py
@@ -1,3 +1,11 @@
+'''
+ELLIORA ORVA LINNISA (2203451)
+METODE BAGI DUA
+No. 2
+1. Buatlah modifikasi fungsi ketika kriteria program berhenti adalah sudah mencapai pada iterasi ke-n
+2. Buatlah modifikasi agar user dapat menginputkan fungsi maupun batas akar dan galatnya
+3. Buatlah modifikasi agar akarnya dapat ditampilkan dalam bentuk grafik
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 
